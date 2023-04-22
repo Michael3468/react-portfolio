8149918ae91ae23917075f4e59024ed9e17fe66f
Mikhail <mikhail3468@yandex.ru>
2023-04-22 07:01:47 +0300

    feat(app.tsx): add Routes and NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Footer, Navbar } from './components';
 import Contacts from './pages/Contacts';
-// import Home from './pages/Home';
-// import Projects from './pages/Projects';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import Projects from './pages/Projects';
 
 import './assets/styles/main.css';
 
 const App = () => (
   <div className="App">
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
 
-    {/* <Home /> */}
-    {/* <Projects /> */}
-    <Contacts />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
 
-    <Footer />
+      <Footer />
+    </BrowserRouter>
   </div>
 );
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,41 +1,54 @@
+import { NavLink } from 'react-router-dom';
+
 import moon from './moon.svg';
 import sun from './sun.svg';
 
 import './styles.css';
 
-const Navbar = () => (
-  <nav className="nav">
-    <div className="container">
-      <div className="nav-row">
-        <a href="./index.html" className="logo">
-          <strong>Portfolio</strong>
-        </a>
+const Navbar = () => {
+  const activeLink = 'nav-list__link nav-list__link--active';
+  const normalLink = 'nav-list__link';
 
-        <button type="button" className="dark-mode-btn">
-          <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-          <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-        </button>
+  return (
+    <nav className="nav">
+      <div className="container">
+        <div className="nav-row">
+          <NavLink to="/" className="logo">
+            <strong>Portfolio</strong>
+          </NavLink>
 
-        <ul className="nav-list">
-          <li className="nav-list__item">
-            <a href="./index.html" className="nav-list__link nav-list__link--active">
-              Home
-            </a>
-          </li>
-          <li className="nav-list__item">
-            <a href="./projects.html" className="nav-list__link">
-              Projects
-            </a>
-          </li>
-          <li className="nav-list__item">
-            <a href="./contacts.html" className="nav-list__link">
-              Contacts
-            </a>
-          </li>
-        </ul>
+          <button type="button" className="dark-mode-btn">
+            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+          </button>
+
+          <ul className="nav-list">
+            <li className="nav-list__item">
+              <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink
+                to="/projects"
+                className={({ isActive }) => (isActive ? activeLink : normalLink)}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink
+                to="/contacts"
+                className={({ isActive }) => (isActive ? activeLink : normalLink)}
+              >
+                Contacts
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
