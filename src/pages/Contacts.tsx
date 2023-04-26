33651e6c68a2349131cde2d8c668c7a8d97650af
Mikhail <mikhail3468@yandex.ru>
2023-04-26 16:29:26 +0300

    refactor(contacts): change contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -9,15 +9,13 @@ const Contacts = () => (
           <p>Moscow, Russia</p>
         </li> */}
         <li className="content-list__item">
-          <h2 className="title-2">Telegram / WhatsApp</h2>
-          <p>
-            <a href="tel:+79051234567">+7 (905) 123-45-67</a>
-          </p>
+          <h2 className="title-2">Telegram</h2>
+          <a href="https://t.me/rhccoder">Click to send message</a>
         </li>
         <li className="content-list__item">
           <h2 className="title-2">Email</h2>
           <p>
-            <a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a>
+            <a href="mailto:mikhail3468@yandex.ru">mikhail3468@yandex.ru</a>
           </p>
         </li>
       </ul>
