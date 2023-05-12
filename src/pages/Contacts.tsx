bae201005c4cd4f43455ffd0e07da989b85e964a
Mikhail <mikhail3468@yandex.ru>
2023-05-12 16:09:30 +0300

    refactor(titles): add AnimationScale3d to 'Contacts' and 'Projects' titles

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,34 +1,43 @@
+/* eslint-disable react/no-array-index-key */
 import { styles } from '../assets/constants';
+import AnimationScale3d from '../stories/components/Animations/AnimationScale3d';
 
-const Contacts = () => (
-  <main className="section">
-    <div className="container">
-      <h1 className="title-1" style={{ textShadow: `${styles.mainTheme.textShadow}` }}>
-        Contacts
-      </h1>
+const Contacts = () => {
+  const contactsString = 'Contacts'.split('');
 
-      <ul className="content-list">
-        {/* <li className="content-list__item">
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1" style={{ textShadow: `${styles.mainTheme.textShadow}` }}>
+          {/* Contacts */}
+          {contactsString.map((letter, index) => (
+            <AnimationScale3d key={index}>{letter === ' ' ? '\u00A0' : letter}</AnimationScale3d>
+          ))}
+        </h1>
+
+        <ul className="content-list">
+          {/* <li className="content-list__item">
           <h2 className="title-2">Location</h2>
           <p>Moscow, Russia</p>
         </li> */}
-        <li className="content-list__item">
-          <h2 className="title-2 text-shadow">Telegram</h2>
-          <a href="https://t.me/rhccoder" className="text-shadow">
-            Click to send message
-          </a>
-        </li>
-        <li className="content-list__item">
-          <h2 className="title-2 text-shadow">Email</h2>
-          <p>
-            <a href="mailto:mikhail3468@yandex.ru" className="text-shadow">
+          <li className="content-list__item">
+            <h2 className="title-2 text-shadow">Telegram</h2>
+            <a href="https://t.me/rhccoder" className="text-shadow">
               Click to send message
             </a>
-          </p>
-        </li>
-      </ul>
-    </div>
-  </main>
-);
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2 text-shadow">Email</h2>
+            <p>
+              <a href="mailto:mikhail3468@yandex.ru" className="text-shadow">
+                Click to send message
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
 
 export default Contacts;
