602a387bba0e9701555314bcaef50da4902c3209
Mikhail <mikhail3468@yandex.ru>
2023-10-30 18:09:34 +0300

    feat: add vue landing (#6)

--- src/shared/constants/testTasksList.ts
+++ src/shared/constants/testTasksList.ts
@@ -1,17 +1,29 @@
-import project20Big from '../assets/img/testProjects/20-big.jpg';
-import project20 from '../assets/img/testProjects/20.jpg';
+import project21Big from '../assets/img/testProjects/21-big.jpg';
+import project21 from '../assets/img/testProjects/21.jpg';
+import project29Big from '../assets/img/testProjects/29-big.jpg';
+import project29 from '../assets/img/testProjects/29.jpg';
 import { TProjects } from '../types';
 
 const testTasksList: TProjects[] = [
   {
-    id: 20,
+    id: 29,
     title: 'React Landing',
     skills: 'React, Typescript, Vite',
-    img: project20,
-    bigImg: project20Big,
+    img: project29,
+    bigImg: project29Big,
     background: 'rgb(255, 178, 32)',
-    gitHubLink: 'https://github.com/WhiteDevilMan/astrum-entertainment-test',
+    gitHubLink: 'https://github.com/Michael3468/astrum-entertainment-test',
     liveDemo: 'https://astrum-entertainment-test.vercel.app/',
+  },
+  {
+    id: 21,
+    title: 'Vue Landing',
+    skills: 'Vue, Typescript, Vite',
+    img: project21,
+    bigImg: project21Big,
+    background: 'rgb(238, 147, 67)',
+    gitHubLink: 'https://github.com/Michael3468/vue-test-task',
+    liveDemo: 'https://vue-test-task-rhccoder.vercel.app/',
   },
 ];
 
