e446a7f63e6751c8ac1b5b2f2da64f96d4005e6b
Mikhail <mikhail3468@yandex.ru>
2023-10-31 09:02:47 +0300

    feat: add Vue TODO project (#6)

--- src/shared/constants/testTasksList.ts
+++ src/shared/constants/testTasksList.ts
@@ -1,5 +1,7 @@
 import project21Big from '../assets/img/testProjects/21-big.jpg';
 import project21 from '../assets/img/testProjects/21.jpg';
+import project22Big from '../assets/img/testProjects/22-big.jpg';
+import project22 from '../assets/img/testProjects/22.jpg';
 import project29Big from '../assets/img/testProjects/29-big.jpg';
 import project29 from '../assets/img/testProjects/29.jpg';
 import { TProjects } from '../types';
@@ -16,12 +18,22 @@ const testTasksList: TProjects[] = [
     liveDemo: 'https://astrum-entertainment-test.vercel.app/',
   },
   {
+    id: 22,
+    title: 'Vue TODO',
+    skills: 'Vue, Typescript, Vite',
+    img: project22,
+    bigImg: project22Big,
+    background: 'rgb(63, 251, 11)',
+    gitHubLink: 'https://github.com/Michael3468/vue3-test-task-2',
+    liveDemo: 'https://vue3-test-task-2.vercel.app',
+  },
+  {
     id: 21,
     title: 'Vue Landing',
     skills: 'Vue, Typescript, Vite',
     img: project21,
     bigImg: project21Big,
-    background: 'rgb(238, 147, 67)',
+    background: 'rgb(94, 69, 185)',
     gitHubLink: 'https://github.com/Michael3468/vue-test-task',
     liveDemo: 'https://vue-test-task-rhccoder.vercel.app/',
   },
